06a3b0172f96b9b10f484d55b65adcdc8a2ade62
patriciasouzas <patriciasouzaa89@gmail.com>
2023-11-22 20:52:28 -0300

    Adicionar arquivos de projeto.

---
+++ FundamentosCsharp.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var frase = "     C# é uma linguagem moderna e versário." +
+	"Com C# você consegue desenvolver para web, desktop, jogos," +
+	"Mobile, entre outros. ";
+
+var tamanho = frase.Length;
+
+var vazio = string.Empty;
+
+var paragrafoMinusculo = frase.ToLower();
+var paragrafoMaiusculo = frase.ToUpper();
+
+var frases = frase.Split('.');
+
+var fraseTrim = frase.Trim();
+var fraseTrimEnd = frase.TrimEnd();
+var fraseTrimStart = frase.TrimStart();
+
+var isNullOrWhiteSpace = string.IsNullOrWhiteSpace(frase);
+
