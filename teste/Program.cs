40ba493937876f34f700479cb45075842186d6cc
patriciasouzas <patriciasouzaa89@gmail.com>
2023-11-22 23:02:04 -0300

    treinando tratamento de exceção

---
+++ teste/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var sete = "sete";
+string valorNull = null;
+var longValue = long.MaxValue.ToString();
+
+try
+{
+	var formatException = int.Parse(sete);
+	var argumentNullException = int.Parse(valorNull);
+	var overFlowException = int.Parse(longValue);
+}
+catch(FormatException ex)
+{
+	Console.WriteLine($"Format Exception: {ex.Message}");
+}
+catch(ArgumentNullException ex)
+{
+	Console.WriteLine($"Argumento Null Exception: {ex.Message}");
+}
+catch(OverflowException ex)
+{
+	Console.WriteLine($"Overflow Exception: {ex.Message}");
+}
+finally
+{
+	Console.WriteLine("Este código é executado sempre.");
+}
+
+Console.ReadKey();
